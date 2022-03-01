db301461741c2c6011ffe7ef0ee1259090578d30
Camille Dauchy <camille.dauchy@2020.icam.fr>
2022-03-01 15:52:47 +0100

    model added has_many

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+  has_many :stocks, :publications, :company_tags, :recommendations, :favorites, :reviews, :track_items
+  has_many :tags, through: :company_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :track_items, :favorites, :likes, :reviews
+  has_many :companies, through: :favorites
 end

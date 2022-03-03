f0d4270465498f309de96a92b0b7b1bb3322c307
Emily Haynes <emi.haynes@icloud.com>
2022-03-03 17:59:53 +0100

    dash edit

--- app/models/track_item.rb
+++ app/models/track_item.rb
@@ -11,5 +11,4 @@ class TrackItem < ApplicationRecord
     user = User.find(favorite.user_id)
     user.track_items.to_a.select { |tracked_company| tracked_company.company == favorite.company }
   end
-
 end

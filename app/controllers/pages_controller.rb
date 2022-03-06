a33355eb1048992abfb92366edef9e81a44e2989
Camille Dauchy <camille.dauchy@2020.icam.fr>
2022-03-06 15:45:49 +0100

    added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
-    @companies = Company.joins(:stocks).order('pourcentage DESC').uniq
-    @top_3_companies = [@companies[0], @companies[1], @companies[2]]
+    @companies = Company.all
+    companies_pourcentage = @companies.map do |company|
+      stock = company.stocks.order('created_at DESC').first
+      { company: company, stock: stock.pourcentage }
+    end
+    @companies_order = companies_pourcentage.sort_by { |h| h[:stock] }
+    @companies_order.reverse!
+    @top_3_companies = [@companies_order[0][:company], @companies_order[1][:company], @companies_order[2][:company]]
     @favorite = Favorite.new
     @favorite_user = Favorite.where('user_id = ?', current_user)
   end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,39 @@ class ProfilesController < ApplicationController
     @profile = current_user
     authorize @profile
     @track_items = TrackItem.new
+    @all_favorite = TrackItem.where('user_id = ?', current_user).select('company_id').group('company_id')
+    @order_invest = best_worth_invest
+  end
+
+  private
+
+  def order_invest
+    all_track = TrackItem.where('user_id = ?', current_user).select('company_id').group('company_id')
+    order_track = all_track.map do |track|
+      company = Company.find(track.company_id)
+      total_gain = company.gains(current_user)
+      { company: track.company_id, gain: total_gain }
+    end
+    tracks = order_track.sort_by { |h| h[:gain] }
+    return tracks.reverse!
+  end
+
+  def best_worth_invest
+    first_part = order_invest.length.fdiv(2).round
+    second_part = order_invest.length - first_part
+    best_invest = []
+    worth_invest = []
+    i = 0
+    j = first_part
+    first_part.times do
+      best_invest.push(order_invest[i])
+      i += 1
+    end
+
+    second_part.times do
+      worth_invest.push(order_invest[j])
+      j += 1
+    end
+    return [best_invest, worth_invest]
   end
 end
